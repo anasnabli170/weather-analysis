216522fc4775318869bf2be8f82e94af42a970e6
anasnabli170 <anasnabli170@gmail.com>
2026-06-19 14:28:19 +0100

    Hometown input

--- weather.py
+++ weather.py
@@ -9,12 +9,22 @@ week_ago=today-timedelta(days=7)
 start_date=week_ago.strftime("%Y-%m-%d")
 end_date=today.strftime("%Y-%m-%d")
 
-url=f"https://api.open-meteo.com/v1/forecast?latitude=36.695443&longitude=10.386358&start_date={start_date}&end_date={end_date}&daily=temperature_2m_max,temperature_2m_min"
-response=requests.get(url)
-data=response.json()
-print (data)
+town=input("Please enter your hometown:")
 
-daily_data=data['daily']
+town_url=f"https://geocoding-api.open-meteo.com/v1/search?name={town}&count=10&language=en&format=json"
+response=requests.get(town_url)
+town_data=response.json()
+print(town_data)
+
+latitude=town_data['results'][0]['latitude']
+longitude=town_data['results'][0]['longitude']
+
+weather_url=f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&start_date={start_date}&end_date={end_date}&daily=temperature_2m_max,temperature_2m_min"
+response=requests.get(weather_url)
+weather_data=response.json()
+print (weather_data)
+
+daily_data=weather_data['daily']
 df=pd.DataFrame({
   'date':daily_data['time'],
   'max_temp':daily_data['temperature_2m_max'],
@@ -31,8 +41,3 @@ if not os.path.exists('data'):
 df.to_csv('data/your_hometown_weather.csv',index=False)
 print(f"average temperature:{df['avg_temp'].mean():.1f}C°")
 print("Data saved to data/your_hometown_weather.csv")
-
-
-
-
-
